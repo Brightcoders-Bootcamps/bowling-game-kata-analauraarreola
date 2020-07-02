eac79a92a8c02caac9f22e23866bdaea628aebd7
Ana Arreola <laurarreola.ana@gmail.com>
2020-07-01 21:09:03 -0500

    Starting algorithm to bowling game

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+gem 'rubocop', '~> 0.86.0', require: false
+gem 'rubycritic', require: false
+gem 'rspec'
+# gem "rails"
---
+++ bowling_game.swift
@@ -0,0 +1,202 @@
+import Foundation
+
+class Turn: CustomStringConvertible {
+    enum FrameEffect {
+        case none
+        case strike
+        case spare
+    }
+
+    
+    var knockedDownPins: Int = 0 {
+        didSet {
+            // We had a strike
+            if knockedDownPins == 10 {
+                frameEffect = .strike
+            }
+            // But if knockedDownPins == availablePins, then we have a spare
+            else if knockedDownPins == availablePins {
+                frameEffect = .spare
+            }
+        }
+    }
+
+    var frameEffect: FrameEffect = .none
+
+    private let availablePins: Int
+    init(withAvailablePins availablePins: Int) {
+        self.availablePins = availablePins
+    }
+
+    func throwBall() {
+        knockedDownPins = availablePins - Int.random(in: 0...availablePins)
+    }
+
+    var description: String {
+        return "\(knockedDownPins)"
+    }
+}
+
+class Frame: CustomStringConvertible {
+    var turns: [Turn] = []
+    var bonus: Int = 0
+    var knockedDownPins: Int = 0
+
+    var score: Int {
+        return initialScore + knockedDownPins + bonus
+    }
+
+    let initialScore: Int
+    init(initialScore: Int = 0) {
+        self.initialScore = initialScore
+    }
+
+    var description: String {
+        return """
+        |---------------------------
+        | Turns: \(turns)
+        |
+        | Score: [ \(score) ]
+        |---------------------------
+        """
+    }
+}
+
+class Game {
+    var gameEnded = false
+
+    var playedTurns: [Turn] = []
+    var lastPlayedTurn: Turn? {
+        return playedTurns.last
+    }
+
+    var frames: [Frame] = []
+
+    var currentlyAvailablePins: Int {
+        let isFirstTurnOfFrame = playedTurns.count % 2 == 0
+        return isFirstTurnOfFrame ? 10 : 10 - (lastPlayedTurn?.knockedDownPins ?? 0)
+    }
+
+    func printScores() {
+        for frame in frames {
+            print(frame)
+        }
+    }
+
+    enum GameNotification: Error {
+        case ended
+        case cannotPlayAnotherTurn
+    }
+
+    func playTurn() throws {
+        if frames.count == 10 || gameEnded {
+            print("You cannot play another turn on this game.")
+            throw GameNotification.cannotPlayAnotherTurn
+        }
+
+        let turn = Turn(withAvailablePins: currentlyAvailablePins)
+        turn.throwBall()
+        playedTurns.append(turn)
+
+      
+        frames = calculateFrames()
+
+        if playedTurns.count == 20, lastPlayedTurn?.frameEffect != .strike {
+            gameEnded = true
+            throw GameNotification.ended
+        }
+    }
+
+    func calculateFrames() -> [Frame] {
+        var frames: [Frame] = []
+
+        
+        var referenceFrame = Frame()
+        var referenceFrameUpdateCounter = 0
+        var skipIndex: Int?
+
+       
+        for (idx, turn) in playedTurns.enumerated() {
+            
+            if let skipIndex = skipIndex, skipIndex == idx {
+                continue
+            }
+
+            let frameInitialScore = frames.last?.score ?? 0
+
+            var bonus = 0
+            switch turn.frameEffect {
+            case .strike:
+    
+                bonus = (idx+1...idx+2).map(scoreForTurn(at:)).reduce(0, +)
+
+                
+                let newFrame = Frame(initialScore: frameInitialScore)
+                newFrame.turns = [turn]
+                newFrame.bonus = bonus
+                newFrame.knockedDownPins = scoreForTurn(at: idx)
+
+                frames.append(newFrame)
+
+                
+                skipIndex = idx
+
+            case .spare:
+             
+                if idx+1 < playedTurns.count {
+                    bonus = scoreForTurn(at: idx+1)
+                }
+
+                let newFrame = Frame(initialScore: frameInitialScore)
+                newFrame.bonus = bonus
+
+                newFrame.turns = [playedTurns[idx - 1], turn]
+
+                
+                newFrame.knockedDownPins = scoreForTurn(at: idx) + scoreForTurn(at: idx - 1)
+                frames.append(newFrame)
+
+            case .none:
+               
+                referenceFrame.knockedDownPins += turn.knockedDownPins
+                referenceFrame.turns.append(turn)
+                referenceFrameUpdateCounter += 1
+
+                
+                if idx < 20 && referenceFrameUpdateCounter == 2 {
+                    let newFrame = Frame(initialScore: frameInitialScore)
+                    newFrame.knockedDownPins = referenceFrame.knockedDownPins
+                    newFrame.turns = referenceFrame.turns
+
+                    frames.append(newFrame)
+
+                    referenceFrame = Frame()
+                    referenceFrameUpdateCounter = 0
+                }
+            }
+        }
+
+        return frames
+    }
+
+    func scoreForTurn(at index: Int) -> Int {
+        if index < playedTurns.count {
+            return playedTurns[index].knockedDownPins
+        }
+
+        return 0
+    }
+}
+
+let game = Game()
+
+for _ in (0..<21) {
+    do {
+        try game.playTurn()
+    } catch {
+        print(error)
+        break
+    }
+}
+
+game.printScores()
